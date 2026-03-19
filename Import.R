1eae0d1e8ccb8aaa18b32406e793ddc6ac4514ce
Péter Kalicz <kaliczp@gmail.com>
2026-03-19 11:29:32 +0100

    One missing measurement

--- Import.R
+++ Import.R
@@ -1,3 +1,6 @@
+## Load package
 library(readxl)
+## Creek
 CreekComponentNames <- as.character(read_excel("PFAS calculation table.xlsx", col_names = FALSE)[2, seq(66,289,5)])
-CreekValues <- as.data.frame(read_excel("PFAS calculation table.xlsx", col_names = FALSE, skip = 20)[1:11,c(1,seq(68,289,5))])
+CreekValues <- as.data.frame(read_excel("PFAS calculation table.xlsx", col_names = FALSE, skip = 20)[1:12,c(1,seq(68,289,5))])
+
