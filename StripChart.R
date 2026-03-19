2b7a9b5b253a7cf01213719b5e6cb5378904aec3
Péter Kalicz <kaliczp@gmail.com>
2026-03-19 12:23:44 +0100

    Levels in the same order, enhanced stripcart

--- StripChart.R
+++ StripChart.R
@@ -1,9 +1,6 @@
 ## Creek1
-Creek1Components <- data.frame(Component = as.factor(rep(CreekComponentNames, each = 2)),
+Creek1Components <- data.frame(Component = factor(rep(CreekComponentNames, each = 2), levels = CreekComponentNames),
                                Value = as.numeric(as.matrix(CreekValues[1:2,-1]))                               )
 
 par(las = 2, mar = c(7, 4.1, 0.1, 0.1))
-stripchart(Creek1Components)
-
 stripchart(Value ~ Component, Creek1Components,pch=1,vertical=T)
-
